82426267708c2b3b32478f05c95682e33bc4da11
hogashi <i1511015@edu.cc.uec.ac.jp>
2019-01-02 01:13:33 +0900

    use sass

--- webpack.config.js
+++ webpack.config.js
@@ -1,46 +1,24 @@
 const path = require('path');
 
-module.exports = [
-  {
-    entry: './src/index.js',
-    output: {
-      filename: 'index.js',
-      path: path.resolve(__dirname, 'public/js')
-    },
-    devtool: 'inline-source-map',
-    module: {
-      rules: [
-        {
-          test: /\.js$/,
-          exclude: /node_modules/,
-          use: {
-            loader: 'babel-loader',
-            options: {
-              presets: ['@babel/preset-env'],
-            },
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'index.js',
+    path: path.resolve(__dirname, 'public/js')
+  },
+  devtool: 'inline-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
           },
         },
-      ],
-    },
+      },
+    ],
   },
-  {
-    entry: './src/index.scss',
-    output: {
-      filename: 'index.css',
-      path: path.resolve(__dirname, 'public/css')
-    },
-    devtool: 'inline-source-map',
-    module: {
-      rules: [
-        {
-          test: /\.scss$/,
-          exclude: /node_modules/,
-          use: [
-            "sass-loader"
-          ],
-        }
-      ],
-    },
-  }
-];
-
+};
