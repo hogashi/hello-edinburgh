33630eae44b038cf8e13a26d5994b8d3c92f8ff0
hogashi <i1511015@edu.cc.uec.ac.jp>
2019-02-09 15:12:17 +0900

    fix service worker, add theme-color meta-tag

--- static/sw.js
+++ static/sw.js
@@ -2,16 +2,22 @@ self.addEventListener('install', e => {
   e.waitUntil(
     caches.open('v1').then(cache => {
       return cache.addAll([
-        '/timeline/',
-        '/js/index.css',
         '/js/index.js',
+        '/css/index.css',
+        '/images/icon.png',
+        '/favicon.ico',
       ]);
     })
   );
 });
 
 self.addEventListener('fetch', e => {
+  if (/^https:\/\/[^\/]*twitter.com/.test(e.request)) {
+    return;
+  }
   e.respondWith(
-    caches.match(e.request)
+    caches.match(e.request).then(res => {
+      return res || fetch(e.request);
+    })
   );
 });
