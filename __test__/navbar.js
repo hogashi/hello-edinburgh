b52ff08977671de0cb2033ebefe7c6e102239174
hogashi <i1511015@edu.cc.uec.ac.jp>
2019-03-17 17:15:59 +0900

    use describe

--- __test__/navbar.js
+++ __test__/navbar.js
@@ -3,11 +3,13 @@ import renderer from 'react-test-renderer';
 
 import Navbar from '../src/components/navbar';
 
-test('Navbar', () => {
-  const component = renderer.create(
-    <Navbar message='message' />
-  );
+describe('Navbar', () => {
+  it('render', () => {
+    const component = renderer.create(
+      <Navbar message='message' />
+    );
 
-  let tree = component.toJSON();
-  expect(tree).toMatchSnapshot();
+    let tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
 });
