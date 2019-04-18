a9b8c7a8eaa899d0a37d31000dafaa2367362d34
hogashi <i1511015@edu.cc.uec.ac.jp>
2019-04-19 01:06:39 +0900

    use env at rack-session-cookie-secret

--- edinburgh.rb
+++ edinburgh.rb
@@ -25,7 +25,7 @@ class Edinburgh < Sinatra::Base
     use Rack::Session::Cookie,
       :key => 'rack.session',
       :expire_after => 60 * 60 * 24 * 30,
-      :secret => Digest::SHA256.hexdigest(rand.to_s)
+      :secret => ENV["RACK_COOKIE_SECRET"]
 
     mime_type :js, 'text/javascript'
     mime_type :css, 'text/css'
